be0d0cab60c42f092e4f6dfcb1c5e8ca44ffc260
JRyan0 <joaoryan10@gmail.com>
2022-08-24 22:30:58 -0300

    SWIPER FUNCIONANDO!!!

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -3,6 +3,23 @@
 <script type="text/javascript" src="script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 <script>
+   var swiper = new Swiper('.swiper', {
+          // If we need pagination
+          pagination: {
+            el: '.swiper-pagination',
+          },
+
+          // Navigation arrows
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+
+          // And if we need scrollbar
+          scrollbar: {
+            el: '.swiper-scrollbar',
+          },
+        });
 
 
 </script>
--- resources/views/hmx/index.blade.php
+++ resources/views/hmx/index.blade.php
@@ -1,49 +1,9 @@
 @extends('base.index')
 
 @section('container')
-<!-- Slider main container -->
-<div class="swiper">
-  <!-- Additional required wrapper -->
-  <div class="swiper-wrapper">
-    <!-- Slides -->
-    <div class="swiper-slide">Slide 1</div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-  </div>
-  <!-- If we need pagination -->
-  <div class="swiper-pagination"></div>
 
-  <!-- If we need navigation buttons -->
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-
-  <!-- If we need scrollbar -->
-  <div class="swiper-scrollbar"></div>
-</div>
-
-
+  @include('base.swiper', ['slide1' => 'images\a.jpg','slide2' => 'images\a.jpg','slide3' => 'images\a.jpg'])
   @include('base.carrinhoClose')
+  @include('base.footer')
 
-  <!-- Use any element to open the sidenav -->
-    <script>
-          var swiper = new Swiper('.swiper', {
-          // If we need pagination
-          pagination: {
-            el: '.swiper-pagination',
-          },
-
-          // Navigation arrows
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-
-          // And if we need scrollbar
-          scrollbar: {
-            el: '.swiper-scrollbar',
-          },
-        });
-
-    </script>
-    @include('base.footer')
 @endsection
